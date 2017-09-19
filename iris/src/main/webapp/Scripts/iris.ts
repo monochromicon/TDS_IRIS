cab004a050c92aa982f60789bfeb1be32aed70c6
Graham Barber <grahammbarber@gmail.com>
2017-09-19 14:36:50 -0700

    Updated features to ES2015-ish

--- iris/src/main/webapp/Scripts/iris.ts
+++ iris/src/main/webapp/Scripts/iris.ts
@@ -1,50 +1,51 @@
 let isIrisReady = false
-//Adding this onto TDS for now so it is available in the dictionary handler.
-let irisUrl = location.href
+// Adding this onto TDS for now so it is available in the dictionary handler.
+const irisUrl = location.href
 // we load one page in advance, but we don't want that to cause a cascade of page show/load
 Blackbox.getConfig().preventShowOnLoad = true
 Blackbox.getConfig().baseUrl = irisUrl
 ContentManager.Dialog.urlFrame = 'Pages/DialogFrame.aspx'
-//This sets read only mode on the content manager disabling the answer entry areas.
+// This sets read only mode on the content manager disabling the answer entry areas.
 CM.setReadOnly(true)
 
 // Functions that are used by toolbar buttons
 
-//Calculator
-let calculatorBtn = function(ev) {
-  let currentPage = ContentManager.getCurrentPage()
+// Calculator
+function calculatorBtn(ev) {
+  const currentPage = ContentManager.getCurrentPage()
   if (currentPage) {
     Calculator.toggle()
   }
 }
-let comments = function(ev) {
-  let currentPage = ContentManager.getCurrentPage()
-  let currentItem = ContentManager.getCurrentEntity()
+
+function comments(ev) {
+  const currentPage = ContentManager.getCurrentPage()
+  const currentItem = ContentManager.getCurrentEntity()
 
   if (currentPage && TDS.Notes && currentItem) {
-    let itemId = getItemId(currentItem)
+    const itemId = getItemId(currentItem)
     TDS.Notes.open({ id: itemId, type: TDS.Notes.Types.TextArea })
   }
 }
 
-//Global Notes
-let globalNotesBtn = function(ev) {
-  let currentPage = ContentManager.getCurrentPage()
+// Global Notes
+function globalNotesBtn(ev) {
+  const currentPage = ContentManager.getCurrentPage()
   if (currentPage && TDS.Notes) {
     TDS.Notes.open()
   }
 }
 
-//Masking
-let showMask = function(ev) {
-  let currentPage = ContentManager.getCurrentPage()
+// Masking
+function showMask(ev) {
+  const currentPage = ContentManager.getCurrentPage()
   if (currentPage) {
     Masking.toggle()
   }
 }
 
-let dictionaryBtn = function(ev) {
-  let currentPage = ContentManager.getCurrentPage()
+function dictionaryBtn(ev) {
+  const currentPage = ContentManager.getCurrentPage()
   if (currentPage) {
     Dictionary.toggle()
   }
@@ -60,34 +61,36 @@ function getItemId(item) {
 function getItemMap(requestedItems) {
   let distinctItemCount = 0
 
-  let itemMap = requestedItems.reduce(function(map, item) {
+  const itemMap = requestedItems.reduce((map, item) => {
     ++distinctItemCount
     map[getItemId(item)] = item
     return map
   }, {})
 
   if (requestedItems.length !== distinctItemCount) {
-    throw new Error('One or more of the requested items appears multiple times in this request.')
+    throw new Error(
+      'One or more of the requested items appears multiple times in this request.'
+    )
   }
 
   return itemMap
 }
 
 function getExistingPage(requestedItems) {
-  let requestedItemCount = Object.keys(requestedItems).length,
-    partialMatches = false,
-    matchedPage = null,
-    matchedItems = null
+  const requestedItemCount = Object.keys(requestedItems).length
+  let partialMatches = false
+  let matchedPage
+  let matchedItems
 
   // go through each page to try matching items
-  CM.getPages().forEach(function(page) {
-    let items = page.getItems(),
-      matches = []
+  CM.getPages().forEach(page => {
+    const items = page.getItems()
+    const matches = []
 
     // check this page for items which are in the current content request
     items.forEach(function(item) {
-      let itemId = getItemId(item),
-        matchedItem = requestedItems[itemId]
+      const itemId = getItemId(item)
+      const matchedItem = requestedItems[itemId]
 
       if (matchedItem) {
         matches.push({
@@ -97,7 +100,10 @@ function getExistingPage(requestedItems) {
       }
     })
 
-    if (matches.length === items.length && items.length === requestedItemCount) {
+    if (
+      matches.length === items.length &&
+      items.length === requestedItemCount
+    ) {
       // exact match, save the page and items
       matchedPage = page
       matchedItems = matches
@@ -120,23 +126,26 @@ function getExistingPage(requestedItems) {
 }
 
 function isGlobalNotesEnabled() {
-  return TDS.getAccommodationProperties().existsAndNotEquals('Global Notes', 'TDS_GN0')
+  return TDS.getAccommodationProperties().existsAndNotEquals(
+    'Global Notes',
+    'TDS_GN0'
+  )
 }
 
 function loadContent(xmlDoc) {
-  if (typeof xmlDoc == 'string') {
+  if (typeof xmlDoc === 'string') {
     xmlDoc = Util.Xml.parseFromString(xmlDoc)
   }
 
   // create array of content json from the xml
-  let deferred = $.Deferred()
-  let contents = CM.Xml.create(xmlDoc)
+  const deferred = $.Deferred()
+  const contents = CM.Xml.create(xmlDoc)
   let content = contents[0]
 
-  let itemMap = getItemMap(content.items)
-  let result = getExistingPage(itemMap)
+  const itemMap = getItemMap(content.items)
+  const result = getExistingPage(itemMap)
 
-  //if the page is already loaded we want to force a reload because the accommodations may have changed.
+  // If the page is already loaded we want to force a reload because the accommodations may have changed.
   if (result.page) {
     // show the page
     TDS.Dialog.hideProgress()
@@ -169,6 +178,7 @@ function loadContent(xmlDoc) {
   if (isGlobalNotesEnabled()) {
     Blackbox.showButton('btnGlobalNotes', globalNotesBtn, true)
   }
+
   if (TDS.getAccommodationProperties().hasCalculator()) {
     Blackbox.showButton('btnCalculator', calculatorBtn, true)
   }
@@ -185,7 +195,7 @@ function loadContent(xmlDoc) {
      If not set it to zero because this may not be the first item we are loading and the zoom level
      may have been set when we loaded an item earlier.
      */
-  let printSize = CM.getAccProps().getPrintSize()
+  const printSize = CM.getAccProps().getPrintSize()
   if (printSize) {
     CM.getZoom().setLevel(printSize, true)
   } else {
@@ -199,12 +209,12 @@ function loadContent(xmlDoc) {
 let loadedDefaultAccommodations = false
 
 function parseAccommodations(segmentId, position, label, segmentEl) {
-  let types = []
+  const types = []
 
   $(segmentEl)
     .find('accommodation')
     .each(function() {
-      let $this = $(this)
+      const $this = $(this)
 
       types.push({
         name: $this.attr('type'),
@@ -221,7 +231,7 @@ function parseAccommodations(segmentId, position, label, segmentEl) {
     })
 
   // clone default accommodations
-  let accs = Accommodations.Manager.getDefault().clone()
+  const accs = Accommodations.Manager.getDefault().clone()
 
   // overwrite with segment-specific accommodations
   accs.importJson({
@@ -231,7 +241,7 @@ function parseAccommodations(segmentId, position, label, segmentEl) {
     types: types
   })
 
-  // se the first segment's accommodations as default
+  // set the first segment's accommodations as default
   if (!loadedDefaultAccommodations) {
     loadedDefaultAccommodations = true
     Accommodations.Manager.setDefault(segmentId)
@@ -242,33 +252,40 @@ function parseAccommodations(segmentId, position, label, segmentEl) {
 
 function loadGroupedContent(xmlDoc) {
   if (CM.getPages().length > 0) {
-    throw new Error('content has already been loaded; cannot load grouped content')
+    throw new Error(
+      'content has already been loaded; cannot load grouped content'
+    )
   }
 
-  if (typeof xmlDoc == 'string') {
+  if (typeof xmlDoc === 'string') {
     xmlDoc = Util.Xml.parseFromString(xmlDoc)
   }
 
   $(xmlDoc)
     .find('segment')
     .each(function() {
-      let segmentId = $(this).attr('id')
+      const segmentId = $(this).attr('id')
 
       // parse accommodations
-      let accommodations = parseAccommodations(segmentId, 'position', 'label', this)
+      const accommodations = parseAccommodations(
+        segmentId,
+        'position',
+        'label',
+        this
+      )
       Accommodations.Manager.add(accommodations)
 
       // parse content
-      let contents = CM.Xml.create(this)
+      const contents = CM.Xml.create(this)
       for (let i = 0; i < contents.length; ++i) {
-        let content = contents[i]
-        let page = CM.createPage(content)
+        const content = contents[i]
+        const page = CM.createPage(content)
 
         // when a page is shown, we want to begin rendering the following page
         page.once('show', function() {
-          let pages = CM.getPages(),
-            nextPageIndex = pages.indexOf(this) + 1,
-            nextPage = pages[nextPageIndex]
+          const pages = CM.getPages()
+          const nextPageIndex = pages.indexOf(this) + 1
+          const nextPage = pages[nextPageIndex]
 
           if (nextPage) {
             nextPage.render()
@@ -278,10 +295,10 @@ function loadGroupedContent(xmlDoc) {
     })
 
   // render the first page, and notify the caller when it is ready
-  let pages = CM.getPages(),
-    pageCount = pages.length,
-    firstPage = pages[0],
-    deferred = $.Deferred()
+  const pages = CM.getPages()
+  const pageCount = pages.length
+  const firstPage = pages[0]
+  const deferred = $.Deferred()
 
   firstPage.once('loaded', function() {
     deferred.resolve()
@@ -298,34 +315,34 @@ function loadGroupedContent(xmlDoc) {
   return deferred.promise()
 }
 
-//function that is passed to Blackbox.changeAccommodations to modify the accommodations
-//in our case we just want to clear out any accommodations that are set.
+// function that is passed to Blackbox.changeAccommodations to modify the accommodations
+// in our case we just want to clear out any accommodations that are set.
 function clearAccommodations(accoms) {
   accoms.clear()
 }
 
-//parses any accommodations from the token, and sets them on the Blackbox.
+// parses any accommodations from the token, and sets them on the Blackbox.
 function setAccommodations(token) {
-  let parsed = JSON.parse(token)
-  //Call changeAccommodations once to reset all accommodations to their default values
+  const parsed = JSON.parse(token)
+  // Call changeAccommodations once to reset all accommodations to their default values
   Blackbox.changeAccommodations(clearAccommodations)
   if (parsed.hasOwnProperty('accommodations')) {
     Blackbox.setAccommodations(parsed['accommodations'])
-    //Call changeAccommodations a second time to apply the new accommodations that were set
-    //by setAccommodations
-    Blackbox.changeAccommodations(function(accoms) {})
+    // Call changeAccommodations a second time to apply the new accommodations that were set
+    // by setAccommodations
+    Blackbox.changeAccommodations(accoms => {return null})
   }
 }
 
 function loadToken(vendorId, token) {
-  let deferred = $.Deferred()
-  blackBoxReady.then(function() {
+  const deferred = $.Deferred()
+  blackBoxReady.then(() => {
     loadContentPromise(vendorId, token)
       .then(function(value) {
         deferred.resolve(value)
       })
-      .catch(function(error) {
-        let errorMsg = 'error: ' + error + ' with loading token ' + token
+      .catch(error => {
+        const errorMsg = `error: ${error} with loading token ${token}`
         console.log(errorMsg)
         deferred.reject(errorMsg)
       })
@@ -333,17 +350,17 @@ function loadToken(vendorId, token) {
   return deferred
 }
 
-let loadContentPromise = function(vendorId, token) {
-  return new Promise(function(resolve, reject) {
+function loadContentPromise(vendorId, token) {
+  return new Promise((resolve, reject) => {
     Messages.set('TDS.WordList.illustration', 'Illustration', 'ENU')
     TDS.Dialog.showProgress()
     setAccommodations(token)
-    let url = irisUrl + '/Pages/API/content/load?id=' + vendorId
+    const url = `${irisUrl}/Pages/API/content/load?id=${vendorId}`
     $.post(url, token, null, 'xml')
-      .then(function(data) {
+      .then(data => {
         loadContent(data).then(resolve)
       })
-      .fail(function(xhr, status, error) {
+      .fail((xhr, status, error) => {
         TDS.Dialog.hideProgress()
         reject(error)
       })
@@ -351,14 +368,14 @@ let loadContentPromise = function(vendorId, token) {
 }
 
 function loadGroupedContentToken(vendorId, token) {
-  let deferred = $.Deferred()
-  blackBoxReady.then(function() {
+  const deferred = $.Deferred()
+  blackBoxReady.then(() => {
     return loadGroupedContentTokenPromise(vendorId, token)
-      .then(function(value) {
+      .then(value => {
         deferred.resolve(value)
       })
-      .catch(function(error) {
-        let errorMsg = 'error: ' + error + ' with loading token ' + token
+      .catch(error => {
+        const errorMsg = `error: ${error} with loading token ${token}`
         console.log(errorMsg)
         deferred.reject(errorMsg)
       })
@@ -366,27 +383,27 @@ function loadGroupedContentToken(vendorId, token) {
   return deferred
 }
 
-let loadGroupedContentTokenPromise = function(vendorId, token) {
-  return new Promise(function(resolve, reject) {
+function loadGroupedContentTokenPromise (vendorId, token) {
+  return new Promise((resolve, reject) => {
     TDS.Dialog.showProgress()
     setAccommodations(token)
-    let url = location.href + '/Pages/API/content/loadContent?id=' + vendorId
+    const url = `${location.href}/Pages/API/content/loadContent?id=${vendorId}`
     $.post(url, token, null, 'json')
-      .then(function(data) {
+      .then(data => {
         loadGroupedContent(data).then(resolve)
       })
-      .fail(function(xhr, status, error) {
+      .fail((xhr, status, error) => {
         TDS.Dialog.hideProgress()
         reject(error)
       })
   })
 }
 
-let blackBoxReady = new Promise(function(resolve) {
+const blackBoxReady = new Promise(resolve => {
   if (isIrisReady) {
     resolve(true)
   } else {
-    Blackbox.events.on('ready', function() {
+    Blackbox.events.on('ready', () => {
       resolve(true)
     })
   }
@@ -395,10 +412,10 @@ let blackBoxReady = new Promise(function(resolve) {
 function setItemResponse(item, response) {
   if (item && item instanceof ContentItem) {
     itemResponseReady(item)
-      .then(function() {
+      .then(() => {
         item.setResponse(response)
       })
-      .catch(function(err) {
+      .catch(err => {
         console.error(err)
       })
   } else {
@@ -415,32 +432,35 @@ function setItemLabel(item, label) {
 }
 
 function setResponse(value) {
-  let entity = CM.getCurrentPage().getActiveEntity()
-  //Begin Hack: 1327 remove <p> from response with prev/next button
+  const entity = CM.getCurrentPage().getActiveEntity()
+  // Begin Hack: 1327 remove <p> from response with prev/next button
   while (
-    value.search('&amp;') != -1 // '&' comes as '&amp;amp;' in response
+    value.search('&amp;') !== -1 // '&' comes as '&amp;amp;' in response
   )
+  {
     value = value.replace('&amp;', '&')
+  }
   value = $('<div/>')
     .html(value)
     .text()
-  while (value.search('<p>') != -1) value = value.replace('<p>', '')
-  while (value.search('</p>') != -1) value = value.replace('</p>', '</br>')
-  //End Hack
+  while (value.search('<p>') !== -1) value = value.replace('<p>', '')
+  while (value.search('</p>') !== -1) value = value.replace('</p>', '</br>')
+  // End Hack
   setItemResponse(entity, value)
 }
 
 function setResponses(itemResponses) {
-  let items = CM.getCurrentPage().getItems()
+  const items = CM.getCurrentPage().getItems()
   itemResponses.forEach(function(itemResponse) {
-    let itemFromPosition, itemFromId
+    let itemFromPosition
+    let itemFromId
     if (typeof itemResponse.position === 'number') {
       itemFromPosition = items[itemResponse.position - 1]
     }
 
     if (itemResponse.id) {
       itemFromId = items.filter(function(item) {
-        let itemId = getItemId(item)
+        const itemId = getItemId(item)
         return itemId === itemResponse.id
       })[0]
     }
@@ -459,14 +479,14 @@ function setResponses(itemResponses) {
   })
 }
 
-//Checks if item response can be set otherwise waits for fired widget instanceReady event
-let itemResponseReady = function(item) {
-  return new Promise(function(resolve, reject) {
+// Checks if item response can be set otherwise waits for fired widget instanceReady event
+let itemResponseReady = item => {
+  return new Promise((resolve, reject) => {
     if (item && CKEDITOR) {
       if (item.isResponseAvailable()) {
         resolve(true)
       } else {
-        CKEDITOR.on('instanceReady', function() {
+        CKEDITOR.on('instanceReady', () => {
           resolve(true)
         })
       }
@@ -477,7 +497,7 @@ let itemResponseReady = function(item) {
 }
 
 function getResponse() {
-  let entity = CM.getCurrentPage().getActiveEntity()
+  const entity = CM.getCurrentPage().getActiveEntity()
   if (entity instanceof ContentItem) {
     return entity.getResponse().value
   }
@@ -485,13 +505,13 @@ function getResponse() {
 }
 
 function getIndexOfCurrentPage() {
-  let currentPage = CM.getCurrentPage(),
-    pages = CM.getPages(),
-    index = pages.indexOf(currentPage)
+  const currentPage = CM.getCurrentPage()
+  const pages = CM.getPages()
+  const index = pages.indexOf(currentPage)
 }
 
 function getNavigability() {
-  let n = {
+  const n = {
     pages: null,
     index: 0,
 
@@ -508,7 +528,8 @@ function getNavigability() {
       this.havePrevPaginatedItem = false
 
       let currentPage = CM.getCurrentPage()
-      ;(this.pages = this.pages || CM.getPages()), (this.index = this.pages.indexOf(currentPage))
+      this.pages = this.pages || CM.getPages()
+      this.index = this.pages.indexOf(currentPage)
 
       this.haveNextPage = this.index < this.pages.length - 1
       this.havePrevPage = this.index > 0
@@ -556,7 +577,7 @@ function showPrev() {
 }
 
 function sendNavUpdate(navigability) {
-  let n = navigability
+  const n = navigability
   XDM(window.parent).post(
     'IRiS:navUpdate',
     n.havePrevPage || n.havePrevPaginatedItem,
